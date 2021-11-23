17ee6d5c61a9c99b92e685e294326227f878db22
Joshua Ramos <auhsojsomar@gmail.com>
2021-11-23 19:55:40 +0800

    to be fixed

--- csv.js
+++ csv.js
@@ -1,12 +1,19 @@
 import fs from "fs";
 import csv2json from "csvjson-csv2json";
+import csvMerger from "csv-merger";
 const employee = [];
 
-const csv = fs.readdirSync("./csv");
-csv.map((data) => {
-  const result = fs.readFileSync(`./csv/${data}`, { encoding: "utf-8" });
-  const json = csv2json(result, { parseNumbers: true });
-  employee.push(json);
+const test = csvMerger.merge(["./csv/BK.csv", "./csv/CK.csv"]).then((data) => {
+  fs.appendFileSync("test.txt", data);
 });
 
-fs.writeFileSync("employee.json", JSON.stringify(employee[0]));
+console.log(test);
+
+// const csv = fs.readdirSync("./csv");
+// csv.map((data) => {
+//   const result = fs.readFileSync(`./csv/${data}`, { encoding: "utf-8" });
+//   const json = csv2json(result, { parseNumbers: true });
+//   employee.push(json);
+// });
+
+// fs.writeFileSync("employee.json", JSON.stringify(employee));
