2d8c3166d0856b7ede35e60f4b2fd291e909bc47
Joshua Ramos <auhsojsomar@gmail.com>
2021-11-24 16:19:29 +0800

    update bat files

--- run.bat
+++ run.bat
@@ -4,7 +4,7 @@ echo [2] Convert image
 
 set /p command="Process: "
 
-IF %command% == 1 set /p sbu="CSV Filename"
+IF %command% == 1 set /p sbu="CSV Filename: "
 node index %sbu%
 IF %command% == 2 node convert
 
